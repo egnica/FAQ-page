0548b5c973d185aeaf210e59f6ac59189f9fe0c5
egnica <46502133+egnica@users.noreply.github.com>
2024-06-24 15:45:22 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,57 @@
 import {useState} from "react";
 import List from "./list.json";
+import {Markup} from "interweave";
 
 import "./App.css";
+
+//https://interweave.dev/docs/
 
 function App() {
 	const [selectedIndex, setIndex] = useState(null);
 
 	const clickHandler = (index) => {
-		setIndex(index);
+		selectedIndex == index ? setIndex(null) : setIndex(index);
 	};
 
 	return (
 		<>
-			{List.faq.map((item, index) => {
-				return (
-					<div key={item.key}>
-						<div onClick={() => clickHandler(index)}>{item.question}</div>
-						{selectedIndex == item.key && <div>{item.answer}</div>}
-					</div>
-				);
-			})}
+			<div className='agenda-image-div'>
+				<div style={{padding: "5%"}}>
+					<h1 style={{marginTop: "60px"}}>FAQ&apos;s</h1>
+				</div>
+			</div>
+
+			<div className='list-contain'>
+				{List.faq.map((item, index) => {
+					return (
+						<div className='outline' key={item.key}>
+							<div onClick={() => clickHandler(index)} className='item-contain'>
+								<p> {item.question}</p>
+
+								{selectedIndex === index ? (
+									<img
+										style={{margin: "auto"}}
+										width='30'
+										src='https://www.barlowresearch.com/BusinessBankingConference_2024/images//minus.svg'
+									/>
+								) : (
+									<img
+										width='50'
+										style={{margin: "auto"}}
+										src='https://www.barlowresearch.com/BusinessBankingConference_2024/images/plus.svg'
+									/>
+								)}
+							</div>
+
+							{selectedIndex === index && (
+								<div className='fade_in'>
+									<Markup content={item.answer} />
+								</div>
+							)}
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 }
