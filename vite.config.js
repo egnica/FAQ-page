6d21b8f2e39311b293d439b6d773aebbff86356e
egnica <46502133+egnica@users.noreply.github.com>
2024-06-24 16:19:35 -0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "https://www.barlowresearch.com/BusinessBankingConference_2024/faq",
+	plugins: [react()],
+});
